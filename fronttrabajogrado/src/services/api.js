2d39d88283268874b2f9464857122f2b502d7e51
djvalderrama67 <100389591+djvalderrama67@users.noreply.github.com>
2023-11-22 22:14:25 -0500

    Update api.js

--- fronttrabajogrado/src/services/api.js
+++ fronttrabajogrado/src/services/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+// Reemplaza 'tu-backend.herokuapp.com' con la URL de tu aplicación backend en Heroku
 const API_URL = 'https://trasteat-bfdad022aa39.herokuapp.com';
 
 export default axios.create({
@@ -8,3 +9,4 @@ export default axios.create({
         'Content-Type': 'application/json'
     }
 });
+
